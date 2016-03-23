3469534f408d1f3f370bcfc4adb1ea415e9e8a2a
jared beckler and nic netzel <jared.beckler@gmail.com>
2016-03-23 13:15:57 -0700

    add functionality for removing pint from count when one is sold

--- app/sell-pint.component.ts
+++ app/sell-pint.component.ts
@@ -4,21 +4,20 @@ import {Keg} from './keg.model';
 @Component({
   selector: 'sell-pint',
   inputs: ['keg'],
-  outputs: [], // MUST MATCH W/ PUBLIC PROPERTY BELOW THAT INSTANTIATES THE EventEmitter
+  outputs: ['pintSold'], // MUST MATCH W/ PUBLIC PROPERTY BELOW THAT INSTANTIATES THE EventEmitter
   template: `
   <button (click)="sellPint(keg)" class="btn btn-warning btn-xs">Sell a Pint</button>
   `
 })
 
 export class SellPintComponent {
-  // public keg: Keg;
-  // public onPintSold: EventEmitter<Keg>;
-  // public remainingPints: number = this.keg.pintsLeft;
-  // constructor(){
-  //   this.onPintSold = new EventEmitter();
-  // }
-  // sellPint(keg){
-  //   var pintsLeft = this.remainingPints - 1;
-  //   this.onPintSold.emit(keg);
-  // }
+  public keg: Keg;
+  public pintSold: EventEmitter<Keg>;
+  constructor(){
+    this.pintSold = new EventEmitter();
+  }
+  sellPint(keg){
+    keg.pintsLeft = keg.pintsLeft - 1;
+    this.pintSold.emit(keg);
+  }
 }
